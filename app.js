8fe554e6b16bf11ba4980065ad717b9ee77f46e1
Aaron Purtill <aaronpurt@users.noreply.github.com>
2018-09-15 19:51:00 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "Retail Sales - App.js file",
+  "description": "A simple application that displays api data in charts/tables",
+  "repository": "https://github.com/aaronpurt/retail-sales",
+  "logo": "https://node-js-sample.herokuapp.com/node.png",
+  "keywords": ["node", "express", "react"]
+}
